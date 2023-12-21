f5865f108648edf189a3f4a15ad77b21ba436e7c
TrangNguyen18 <thutrang277.neu@gmail.com>
2023-12-21 16:33:50 +0700

    Trang

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,8 @@
+const pool = require('../config/pgConfig');
+const connection = require('../config/pgConfig');
 const getHomepage = (req, res) => {
     res.render('home.ejs');
+    
 };
 const addUser = (req, res) => {
     res.render('user.ejs');
@@ -7,8 +10,14 @@ const addUser = (req, res) => {
 const forgotPw = (req, res)=>{
     res.send ('Do you want to reset pw ?')
 };
-const postAddNewUser = (req, res)=>{
-    console.log('>>> check response:', req.body)
-    res.send ('Create user successfully')
-};
+const postAddNewUser = async(req, res)=>{
+    let email = req.body.email;
+    let name = req.body.username;
+    console.log('user = ',name, 'email =', email);
+    const sql  = 'Insert into users (name, email) values (? ,?)';
+    const [results, fields] = connection.query(sql,[name, email]);
+   // console.log(results);
+    //res.send('Tao user thanh cong')
+}
 module.exports={getHomepage, addUser ,postAddNewUser, forgotPw};
+
